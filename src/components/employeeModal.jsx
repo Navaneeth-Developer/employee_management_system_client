c9b766bb924e6bc7588f7e0d7ab37c83f037f1e5
Navaneeth-Developer <navaneeth0906@gmail.com>
2025-03-12 03:07:39 +0530

    Toastify notification added.

--- src/components/employeeModal.jsx
+++ src/components/employeeModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Modal, Box, TextField, Button } from "@mui/material";
-
+import { toast, ToastContainer } from "react-toastify";
 const modalStyle = {
   position: "absolute",
   top: "50%",
@@ -32,47 +32,59 @@ const EmployeeModal = ({ open, onClose, onSave, initialData = {} }) => {
   };
 
   const handleSubmit = () => {
-    onSave(employee);
-    onClose();
+    console.log("employee==>>", employee, !employee.name);
+
+    if (
+      !employee.name ||
+      !employee.email ||
+      !employee.job_title ||
+      !employee.salary
+    ) {
+      toast.error("Please fill all the fields!");
+    } else {
+      onSave(employee);
+      onClose();
+    }
   };
 
   return (
-    <Modal open={open} onClose={onClose}>
-      <Box sx={modalStyle}>
-        <h2>{initialData?.id ? "Edit Employee" : "Add Employee"}</h2>
-        <TextField
-          label="Name"
-          name="name"
-          value={employee.name}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Email"
-          name="email"
-          value={employee.email}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Job Title"
-          name="job_title"
-          value={employee.job_title}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Salary"
-          name="salary"
-          value={employee.salary}
-          onChange={handleChange}
-          fullWidth
-          margin="normal"
-        />
-        {/* <TextField
+    <>
+      <Modal open={open} onClose={onClose}>
+        <Box sx={modalStyle}>
+          <h2>{initialData?.id ? "Edit Employee" : "Add Employee"}</h2>
+          <TextField
+            label="Name"
+            name="name"
+            value={employee.name}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Email"
+            name="email"
+            value={employee.email}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Role"
+            name="job_title"
+            value={employee.job_title}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Salary"
+            name="salary"
+            value={employee.salary}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          {/* <TextField
           label="Joined Date"
           name="joinedDate"
           type="date"
@@ -82,11 +94,16 @@ const EmployeeModal = ({ open, onClose, onSave, initialData = {} }) => {
           margin="normal"
           InputLabelProps={{ shrink: true }}
         /> */}
-        <Button onClick={handleSubmit} variant="contained" sx={{ mt: 2 }}>
-          Save
-        </Button>
-      </Box>
-    </Modal>
+          <Button onClick={handleSubmit} variant="contained" sx={{ mt: 2 }}>
+            Save
+          </Button>
+          <Button onClick={onClose} variant="contained" sx={{ mt: 2, ml: 1 }}>
+            Close
+          </Button>
+        </Box>
+      </Modal>
+      <ToastContainer />
+    </>
   );
 };
 
